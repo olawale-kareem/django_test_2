1de4592f914f427b20752111b0038407e6843847
olawale-kareem <olawale.kareem@decagon.dev>
2021-08-18 05:07:21 +0100

    The template finishing, medias and static settings added on the project settings

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -2,7 +2,15 @@
 from django.contrib import admin
 from django.urls import  include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+ 
+
 urlpatterns = [
     path('', include('portfolio_website.urls')),
     path('admin/', admin.site.urls)
 ]
+
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+ 
--- portfolio/portfolio_website/models.py
+++ portfolio/portfolio_website/models.py
@@ -6,6 +6,6 @@ class Course(models.Model):
     image = models.ImageField(upload_to = 'images/')
     summary = models.CharField(max_length=200)
 
-    def __str__(self):
+    def __str__(self):  
         return self.summary
   
